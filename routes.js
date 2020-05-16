15607f98a94bde6f2667c069ecaa5e6a92ebda3a
Nikolas Setiawan <nikolashu16@gmail.com>
2020-05-17 03:02:52 +0700

    menghapus data sparepart

--- routes.js
+++ routes.js
@@ -1,8 +1,21 @@
 'use strict';
-
-module.exports = function(app) {
+module.exports = function(app){
     var jsonku = require('./controller');
-
+    
     app.route('/')
-    .get(jsonku.index);
-};
+   .get(jsonku.index);
+   app.route('/tampil')
+    .get(jsonku.tampildatasparepart);
+    app.route('/tampil/:id')
+    .get(jsonku.tampildatasparepartid);
+    app.route('/tampilmontir/:id')
+    .get(jsonku.tampildatamontirid);
+    app.route('/tambahsparepart')
+    .post(jsonku.tambahsparepart);
+    app.route('/ubahsparepart')
+    .put(jsonku.ubahsparepart);
+
+
+    app.route('/hapussparepart')
+    .delete(jsonku.hapusSparepart);
+}
