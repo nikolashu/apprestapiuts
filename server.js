2551ecf0a7cef8982eb22628b125c66f9582c7b3
Nikolas Setiawan <nikolashu16@gmail.com>
2020-05-17 02:59:24 +0700

    registrasi berhasil

--- server.js
+++ server.js
@@ -1,16 +1,21 @@
 const express = require('express');
 const bodyParser = require('body-parser');
+
+var morgan = require('morgan');
 const app = express();
-const port = 3000;
 
 //parse application/json
 app.use(bodyParser.urlencoded({extended: true}));
 app.use(bodyParser.json());
+app.use(morgan('dev'));
 
 //panggil routes
 var routes = require('./routes');
 routes(app);
 
-app.listen(port, (err) => {
-    console.log(`Server started on port : ` + port);
-});
+//daftarkan menu rotes dari index
+app.use('/auth', require('./middleware'));
+
+ app.listen(3000,() => {
+     console.log('Server Started on port 3000');
+ });
